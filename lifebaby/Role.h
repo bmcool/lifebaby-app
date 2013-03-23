7f8c8482a1d20632ed506615c6b6ed99cdeb7a0f
Zephyr Lin <zephyrlin@coevo.com.tw>
2013-03-24 02:27:04 +0800

    [add] first commit

---
+++ lifebaby/Role.h
@@ -0,0 +1,15 @@
+//
+//  Role.h
+//  lifebaby
+//
+//  Created by Lin Chi-Cheng on 13/3/23.
+//  Copyright (c) 2013年 lifebaby. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface Role : NSObject
+
+@property (assign, nonatomic) NSInteger numSteps;
+
+@end
---
+++ lifebaby/Role.m
@@ -0,0 +1,15 @@
+//
+//  Role.m
+//  lifebaby
+//
+//  Created by Lin Chi-Cheng on 13/3/23.
+//  Copyright (c) 2013年 lifebaby. All rights reserved.
+//
+
+#import "Role.h"
+
+@implementation Role
+
+
+
+@end
