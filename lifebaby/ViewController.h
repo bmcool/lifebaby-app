33d89bfedd731d7c0e6ac570e17d5e3a97673e3a
Zephyr Lin <zephyrlin@coevo.com.tw>
2013-03-23 22:57:39 +0800

    Initial Commit

---
+++ lifebaby/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  lifebaby
+//
+//  Created by Lin Chi-Cheng on 13/3/23.
+//  Copyright (c) 2013年 lifebaby. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
---
+++ lifebaby/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  lifebaby
+//
+//  Created by Lin Chi-Cheng on 13/3/23.
+//  Copyright (c) 2013年 lifebaby. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
